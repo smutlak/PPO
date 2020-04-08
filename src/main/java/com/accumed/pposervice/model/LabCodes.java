4a22c5b2ec688b5c9c82567ff09972585364a1aa
smutlak <smutlak@SAMEER.accumedSY.ae>
2020-04-08 14:32:59 +0300

    5 April 2020

---
+++ src/main/java/com/accumed/pposervice/model/LabCodes.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.accumed.pposervice.model;
+
+import com.haad.ClaimSubmission;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ *
+ * @author smutlak
+ */
+@Entity
+@Table(
+        name = "labcodes",
+        indexes = {
+            @Index(name = "accounttransaction_INDX_0", columnList = "code")})
+@NamedQueries({
+    @NamedQuery(name = "LabCodes.findAll", query = "SELECT a FROM LabCodes a")})
+
+@XmlRootElement(name = "Files")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "code",
+    "desc"
+})
+public class LabCodes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @XmlTransient
+    private Long id;
+
+    @Column(name = "code", nullable = false)
+    @XmlElement(name = "code")
+    private String code;
+
+    @Column(name = "desc", nullable = false, length=2048)
+    @XmlElement(name = "desc")
+    private String desc;
+
+   
+    public LabCodes() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+}
